63cd5e550fedea213d9535484c4a361d1a125182
xxori <ori@kek.church>
2023-11-16 21:37:02 +1030

    add java explicitly (its already a dep of tex)

--- mac/default.nix
+++ mac/default.nix
@@ -101,6 +101,7 @@
         zola
         flutter
         cocoapods
+        jdk19_headless
         # # It is sometimes useful to fine-tune packages, for example, by applying
         # # overrides. You can do that directly here, just don't forget the
         # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
